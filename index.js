6d62bb1339d8abd7854464650b9a38954646b659
Tom Hummel <tohu@hey.com>
2021-06-02 20:48:15 -0700

    fix before/after state variable naming

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ function roll () {
   return 1 + Math.floor(Math.random() * 6)
 }
 
-function shoot (preState, dice) {
+function shoot (before, dice) {
   const sortedDice = dice.sort()
 
-  const postState = {
+  const after = {
     die1: sortedDice[0],
     die2: sortedDice[1],
     diceSum: dice.reduce((m, r) => { return m + r }, 0)
   }
 
-  if (preState.isComeOut) {
-    if ([2, 3, 12].indexOf(postState.diceSum) !== -1) {
-      postState.result = 'comeout loss'
-      postState.isComeOut = true
-    } else if ([7, 11].indexOf(postState.diceSum) !== -1) {
-      postState.result = 'comeout win'
-      postState.isComeOut = true
+  if (before.isComeOut) {
+    if ([2, 3, 12].indexOf(after.diceSum) !== -1) {
+      after.result = 'comeout loss'
+      after.isComeOut = true
+    } else if ([7, 11].indexOf(after.diceSum) !== -1) {
+      after.result = 'comeout win'
+      after.isComeOut = true
     } else {
-      postState.result = 'point set'
-      postState.isComeOut = false
-      postState.point = postState.diceSum
+      after.result = 'point set'
+      after.isComeOut = false
+      after.point = after.diceSum
     }
   }
 
-  return postState
+  return after
 }
 
 module.exports = {
