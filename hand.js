74160542666868755d21d212a94fafa307953ae6
Tom Hummel <tohu@hey.com>
2021-06-03 22:08:16 -0700

    Add multiple hand session simulation, driven by cli arg # of hands

--- hand.js
+++ hand.js
@@ -2,6 +2,78 @@
 
 const { playHand } = require('./index.js')
 
-const hand = playHand()
+const numHands = parseInt(process.argv.slice(2)[0], 10)
 
-console.table(hand)
+console.log(`Simulating ${numHands} Craps Hand(s)`);
+
+const results = {
+  handCount: 0,
+  rollCount: 0,
+  pointsSet: 0,
+  pointsWon: 0,
+  comeOutWins: 0,
+  comeOutLosses: 0,
+  netComeOutWins: 0,
+  neutrals: 0,
+  dist: {
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0,
+    12: 0
+  }
+}
+
+const hands = []
+
+for (let i = 0; i < numHands; i++) {
+  const hand = playHand()
+  hands.push(hand)
+
+  hand.reduce((memo, roll) => {
+    memo.rollCount++
+    memo.dist[roll.diceSum]++
+
+    switch (roll.result) {
+      case 'neutral':
+        memo.neutrals++
+        break
+      case 'point set':
+        memo.pointsSet++
+        break
+      case 'point win':
+        memo.pointsWon++
+        break
+      case 'comeout win':
+        memo.comeOutWins++
+        memo.netComeOutWins++
+        break
+      case 'comeout loss':
+        memo.comeOutLosses++
+        memo.netComeOutWins--
+        break
+    }
+
+    return memo
+  }, results)
+}
+
+results.handCount = hands.length
+
+console.log('\nDice Roll Distribution')
+console.table(results.dist)
+delete results.dist
+console.log('\nSession Summary')
+console.table(results)
+
+console.log('\nHands')
+hands.forEach((hand, handNum) => {
+  console.log(`\nHand: ${handNum + 1}`)
+  console.table(hand)
+})
